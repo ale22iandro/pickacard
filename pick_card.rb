729a8e9a17daac379f458bff1b20615cce101540
ale22iandro <ale22iandro@gmail.com>
2021-09-03 10:30:44 -0700

    scnd

--- pick_card.rb
+++ pick_card.rb
@@ -4,3 +4,4 @@ suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Кре
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
 puts "#{values.sample} of #{suits.sample}"
+gets
